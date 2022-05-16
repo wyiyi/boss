825d2e3ad98f9c8a0e9d6507c2b4da801fc64107
w_yi.neu <w_yi.neu@neusoft.com>
2022-05-16 17:08:06 +0800

    销售统计demo

--- pages/sale/sale.js
+++ pages/sale/sale.js
@@ -1,83 +1,106 @@
 const sliderWidth = 96;
 
 Page({
-  mixins: [require('../../mixin/common')],
+  mixins: [require("../../mixin/common")],
   data: {
-    array1: ['全部门店', '浑南店', '和平店', '沈河店'],
+    array1: ["全部门店", "浑南店", "和平店", "沈河店"],
     value1: 0,
     value2: 0,
     value3: 0,
     tableHeader: [
       {
-        prop: 'datetime',
-        width: 150,
-        label: '日期',
-        color: '#55C355'
+        prop: "productname",
+        width: 210,
+        label: "商品名称",
+        color: "#55C355",
       },
       {
-        prop: 'sign_in',
-        width: 152,
-        label: '上班时间'
+        prop: "price",
+        width: 180,
+        label: "销售金额",
       },
       {
-        prop: 'sign_out',
-        width: 152,
-        label: '下班时间'
+        prop: "count",
+        width: 180,
+        label: "销售数量",
       },
       {
-        prop: 'work_hour',
-        width: 110,
-        label: '工时'
+        prop: "ratio",
+        width: 180,
+        label: "销售占比",
       },
-      {
-        prop: 'statusText',
-        width: 110,
-        label: '状态'
-      }
     ],
     stripe: true,
     border: true,
     outBorder: true,
     row: [
       {
-          "id": 1,
-          "status": '正常',
-          "datetime": "04-01",
-          "sign_in_time": '09:30:00',
-          "sign_out_time": '18:30:00',
-          "work_hour": 8,
-      }, {
-          "id": 2,
-          "status": '迟到',
-          "datetime": "04-02",
-          "sign_in_time": '10:30:00',
-          "sign_out_time": '18:30:00',
-          "work_hour": 7,
-      }, {
-          "id": 29,
-          "status": '正常',
-          "datetime": "04-03",
-          "sign_in_time": '09:30:00',
-          "sign_out_time": '18:30:00',
-          "work_hour": 8,
-      }, {
-          "id": 318,
-          "status": '休息日',
-          "datetime": "04-04",
-          "sign_in_time": '',
-          "sign_out_time": '',
-          "work_hour": '',
-      }, {
-          "id": 319,
-          "status": '正常',
-          "datetime": "04-05",
-          "sign_in_time": '09:30:00',
-          "sign_out_time": '18:30:00',
-          "work_hour": 8,
-      }
+        id: 1,
+        productname: "999感冒灵",
+        price: "38",
+        count: "2000",
+        ratio: "83%",
+      },
+      {
+        id: 2,
+        productname: "蒲地蓝口服液",
+        price: "45",
+        count: "1500",
+        ratio: "80%",
+      },
+      {
+        id: 3,
+        productname: "江中健胃消食片",
+        price: "10.8",
+        count: "1300",
+        ratio: "75%",
+      },
+      {
+        id: 4,
+        productname: "美林布洛芬混悬液",
+        price: "34.5",
+        count: "800",
+        ratio: "60%",
+      },
+      {
+        id: 5,
+        productname: "红霉素软膏",
+        price: "34.5",
+        count: "500",
+        ratio: "59%",
+      },
+      {
+        id: 6,
+        productname: "医用口罩",
+        price: "18",
+        count: "1600",
+        ratio: "85%",
+      },
+      {
+        id: 7,
+        productname: "奈敏维滴眼液",
+        price: "36",
+        count: "300",
+        ratio: "50%",
+      },
+      {
+        id: 8,
+        productname: "二甲双胍片",
+        price: "48",
+        count: "680",
+        ratio: "65%",
+      },
+      {
+        id: 9,
+        productname: "牛黄解毒片",
+        price: "10",
+        count: "400",
+        ratio: "55%",
+      },
     ],
-    tabs: ['选项一', '选项二', '选项三'],
-    activeIndex: 1,
+    tabs: ["日", "周", "月", "自定义"],
+    selected: 0,
+    activeIndex: 0,
     sliderOffset: 0,
     sliderLeft: 0,
   },
@@ -87,22 +110,40 @@ Page({
     });
   },
   onLoad() {
-    debugger
     const that = this;
     wx.getSystemInfo({
       success(res) {
         that.setData({
-          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
-          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex,
+          sliderLeft:
+            (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset:
+            (res.windowWidth / that.data.tabs.length) * that.data.activeIndex,
         });
       },
     });
   },
-  tabClick(e) {
-    debugger
-    this.setData({
-      sliderOffset: e.currentTarget.offsetLeft,
-      activeIndex: e.currentTarget.id,
-    });
+  //tab框
+  selected: function (e) {
+    console.log(e);
+    let that = this;
+    let index = e.currentTarget.dataset.index;
+    console.log(index);
+    if (index == 0) {
+      that.setData({
+        selected: 0,
+      });
+    } else if (index == 1) {
+      that.setData({
+        selected: 1,
+      });
+    } else if (index == 2) {
+      that.setData({
+        selected: 2,
+      });
+    } else {
+      that.setData({
+        selected: 3,
+      });
+    }
   },
 });
