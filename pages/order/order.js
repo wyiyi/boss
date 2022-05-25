e84a39be2a66e1bb2e10d25bfa6dcde8604d2a58
w_yi.neu <w_yi.neu@neusoft.com>
2022-05-25 16:50:52 +0800

    调整

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,3 +1,19 @@
 Page({
   mixins: [require('../../mixin/common')],
+  viewCopyTextClick: function(){
+    var _this = this
+    wx.setClipboardData({
+      data: _this.data.viewCopyMsg,
+      success: function (res) {
+        wx.getClipboardData({
+          success: function (res) {
+            wx.showToast({
+              title: '复制成功'
+            })
+          }
+        })
+      }
+    })
+  }
+
 });
